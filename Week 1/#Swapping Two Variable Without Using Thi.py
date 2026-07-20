10277739065b9337395bc15213a5c83ceaa3b0c0
Armughan <armughanzeb55@gmail.com>
2026-07-20 15:41:49 +0500

    Task#1

---
+++ Week 1/#Swapping Two Variable Without Using Thi.py
@@ -0,0 +1,17 @@
+#Swapping Two Variable Without Using Third Variable
+
+a = 5
+b = 10
+
+print("Before swapping: a =", a, "b =", b)
+
+a = a + b # a would be 15 now
+b = a - b # b would be 5  
+a = a - b # a would be 10 now
+
+print("After swapping: a =", a, "b =", b)
+
+#Henced Swapped
+
+
+
